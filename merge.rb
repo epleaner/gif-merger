24924609acccc1f4f6826806d8b5a0d4ae04cf42
Loaner4-SF <loaner4-sf@SFs-MacBook-Air.local>
2017-06-27 10:10:06 -0700

    2 gifs can be meergeeeeed - still needs cleanup

--- merge.rb
+++ merge.rb
@@ -6,19 +6,31 @@ OptionParser.new do |opts|
   opts.banner = "Usage: merge.rb [options]"
 
   opts.on('-b', '--background-image PATH', 'Background image') { |v| options[:background_image] = v }
-  opts.on('-c', '--replace-color COLOR', 'Color to replace') { |v| options[:replace_color] = v }
   opts.on('-f', '--foreground-image PATH', 'Background image') { |v| options[:foreground_image] = v }
+  opts.on('-c', '--replace-color COLOR', 'Color to replace') { |v| options[:replace_color] = v }
   opts.on('-s', '--shorten', 'Shorten longer animation') { || options[:shorten] = true }
   opts.on('-n', '--layer-name NAME', 'Layer name') { |v| options[:layer_name] = v }
-
+  opts.on('-d', '--use-dominant-color', 'Replace dominant color') { || options[:use_dominant] = true }
+  opts.on('-v', '--verbose', 'Verbose') { || options[:verbose] = true }
 
 end.parse!
 
 
 merge_helper = MergeHelper.new
 
-transparent_foreground_path = merge_helper.make_transparent(options[:foreground_image], 25, options[:replace_color])
+if(options[:use_dominant])
+  p "Calculating dominant color for #{options[:foreground_image]}" if options[:verbose]
+  replace_color = merge_helper.dominant_color(options[:foreground_image])
+else
+  replace_color = options[:replace_color]
+end
 
-merge_helper.composite_merge(options[:background_image], transparent_foreground_path, options[:layer_name], options[:shorten])
+p "Making #{replace_color} transparent in #{options[:foreground_image]}" if options[:verbose]
+transparent_foreground_path = merge_helper.make_transparent(options[:foreground_image], color: replace_color)
+
+p "Merging #{options[:foreground_image]} and #{options[:background_image]}" if options[:verbose]
+output_path = merge_helper.composite_merge(options[:background_image], transparent_foreground_path, options[:layer_name], shorten: options[:shorten])
 
 `rm #{transparent_foreground_path}`
+
+p "Done! Result in #{output_path}" if options[:verbose]
--- merge_helper.rb
+++ merge_helper.rb
@@ -1,38 +1,93 @@
+require 'pry'
 class MergeHelper
-  def get_length(filepath)
-    length = `identify -format '%s\n' #{filepath} | tail -1`
-    Integer(length)
+  def get_frame_count(filepath)
+    frame_count = `identify -format '%s\n' #{filepath} | tail -1`
+    Integer(frame_count)
   end
 
   def shorten_longer(background_path, foreground_path)
-    background_length = get_length(background_path)
-    foreground_length = get_length(foreground_path)
+    background_frame_count = get_frame_count(background_path)
+    foreground_frame_count = get_frame_count(foreground_path)
 
-    if background_length > foreground_length
-      `convert #{background_path}[0-#{foreground_length}] shortened_#{background_path}`
-
+    if background_frame_count > foreground_frame_count
+      `convert #{background_path}[0-#{foreground_frame_count}] shortened_#{background_path}`
       background_path = "shortened_#{background_path}"
-    elsif background_length < foreground_length
-      `convert #{foreground_path}[0-#{background_length}] shortened_#{foreground_path}`
 
+    elsif background_frame_count < foreground_frame_count
+      `convert #{foreground_path}[0-#{background_frame_count}] shortened_#{foreground_path}`
       foreground_path = "shortened_#{foreground_path}"
     end
 
     [background_path, foreground_path]
   end
 
-  def composite_merge(background_path, foreground_path, layer_name, shorten=false)
-    if shorten
+  def resize_to_match(background_path, foreground_path)
+    background_width, background_height = `convert #{background_path} -ping -format "%wx%h|" info:`.split('|').first.split('x').map(&:to_i)
+    foreground_width, foreground_height = `convert #{foreground_path} -ping -format "%wx%h|" info:`.split('|').first.split('x').map(&:to_i)
+
+    if background_width * background_height > foreground_width * foreground_height
+      `convert #{foreground_path} -coalesce -resize #{background_width}x#{background_height}! resized_#{foreground_path}`
+      foreground_path = "resized_#{foreground_path}"
+    elsif background_width * background_height < foreground_width * foreground_height
+      `convert #{background_path} -coalesce -resize #{foreground_width}x#{foreground_height}! resized_#{background_path}`
+      background_path = "resized_#{background_path}"
+    end
+
+    [background_path, foreground_path]
+  end
+
+  def composite_merge(background_path, foreground_path, layer_name, shorten: false)
+    layer_name = 'merged' if layer_name.nil?
+    output_path = "#{layer_name}.gif"
+
+    if shorten || true
       background_path, foreground_path = shorten_longer(background_path, foreground_path)
     end
 
-    `convert #{background_path} null: #{foreground_path} -gravity center -layers Composite #{layer_name}.gif`
+    background_path, foreground_path = resize_to_match(background_path, foreground_path)
+
+    bg_frame_ranges = get_background_frame_ranges(background_path, foreground_path)
+    `convert #{background_path}[#{bg_frame_ranges}] null: #{foreground_path} -gravity center -layers Composite #{output_path}`
+
+    output_path
   end
 
-  def make_transparent(image_path, fuzz=25, color="#000000")
+  def make_transparent(image_path, fuzz: 10, color: "#000000")
     transparent_image_path = "transparent_#{image_path}"
     `convert #{image_path} -coalesce -fuzz "#{fuzz}%" -transparent "#{color}" miff:- | convert -dispose background - #{transparent_image_path}`
 
     transparent_image_path
+  end
+
+  def dominant_color(image_path)
+    histogram_path = "#{image_path}.histogram"
+    `convert #{image_path}  -format %c -depth 8  histogram:info:#{histogram_path}`
+
+    dominant_color = `sort -n #{histogram_path} | tail -1`
+
+    `rm #{histogram_path}`
+
+    dominant_color.scan(/#[a-zA-Z0-9]*/).first
+  end
+
+  def get_background_frame_ranges(background_path, foreground_path)
+    background_frames = get_frame_count(background_path)
+    foreground_frames = get_frame_count(foreground_path)
+
+    if foreground_frames <= background_frames
+      frame_range = "0-#{foreground_frames - 1}"
+    else
+      bg_frame_length = "0-#{background_frames - 1}"
+      bg_loops_within_fg = (foreground_frames / background_frames).floor
+      frame_range = (0..bg_loops_within_fg - 1).to_a.fill(bg_frame_length).join(",")
+
+      remainder = foreground_frames % background_frames
+      if remainder > 0
+        frame_range += "," if bg_loops_within_fg > 0
+        frame_range += "0-#{remainder - 1}"
+      end
+    end
+
+    frame_range
   end
 end
